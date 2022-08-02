15395545636de2125b0cc4f7816d8b2146b8cc30
Qadireh Moradi <kmg.d117@gmail.com>
2022-08-02 16:42:26 +0430

    edit style

--- src/components/DatePicker.ts
+++ src/components/DatePicker.ts
@@ -128,8 +128,9 @@ export class DatePicker {
       prevButton.addEventListener("click", (e) => {
         this.prevMonth();
     });
-    nextButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" class="v-icon__svg"><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"></path></svg>`;
-    prevButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" class="v-icon__svg"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path></svg>`;
+    
+    nextButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" class="v-icon__svg"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"></path></svg>`;
+    prevButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" class="v-icon__svg"><path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"></path></svg>`;
     nextButton.setAttribute("data-datepicker-next", "");
     prevButton.setAttribute("data-datepicker-prev", "");
     nextButton.setAttribute("data-sys-inherit","")
